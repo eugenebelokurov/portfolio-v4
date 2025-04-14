5cc474082b347d2d7d49c805bd4bebd38f68f563
Eugene Belokurov <eugenebelokurov@Mac.lan>
2025-04-14 10:55:42 +0100

    remove dark mode for cases

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -6,19 +6,19 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       <h1 className="text-3xl font-medium mb-6">{children}</h1>
     ),
     h2: ({ children }) => (
-      <h2 className="text-gray-800 font-bold dark:text-zinc-200 mt-8 mb-4">{children}</h2>
+      <h2 className="text-gray-800 font-bold mt-8 mb-4">{children}</h2>
     ),
     h3: ({ children }) => (
-      <h3 className="text-gray-600 font-semibold dark:text-zinc-200 mt-8 mb-4">{children}</h3>
+      <h3 className="text-gray-600 font-semibold mt-8 mb-4">{children}</h3>
     ),
     p: ({ children }) => (
-      <p className="text-gray-800 dark:text-zinc-300 font-medium leading-snug mb-4">{children}</p>
+      <p className="text-gray-800 font-medium leading-snug mb-4">{children}</p>
     ),
     ol: ({ children }) => (
-      <ol className="text-gray-800 dark:text-zinc-300 list-decimal pl-5 space-y-2 mb-4">{children}</ol>
+      <ol className="text-gray-800 list-decimal pl-5 space-y-2 mb-4">{children}</ol>
     ),
     ul: ({ children }) => (
-      <ul className="text-gray-800 dark:text-zinc-300 list-disc pl-5 space-y-1 mb-4">{children}</ul>
+      <ul className="text-gray-800 list-disc pl-5 space-y-1 mb-4">{children}</ul>
     ),
     li: ({ children }) => (
       <li className="pl-1 leading-snug">{children}</li>
